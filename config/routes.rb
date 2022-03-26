d945afbfa5aeea038dd841e522551c0990b6eb08
Aldo Diaz <asaavedrajob@gmail.com>
2022-03-25 18:11:47 -0600

    RFB Part 6: The Root Route

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,14 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "main#index"
+  # GET / optional
+  # root to: "main#index"
+  # get "/", to: "main#index"
+  # get "", to: "main#index"
 
   # GET /about
   get "about", to: "about#index"
+  # GET /about optional
+  # get "/about", to: "about#index"
 end
