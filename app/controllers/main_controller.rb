03d1fdb10c3148ec0517590a8340382bf2129fe4
Aldo Diaz <asaavedrajob@gmail.com>
2022-03-25 19:01:58 -0600

    RFB Part 10: Flash messages

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,4 +1,9 @@
 class MainController < ApplicationController
   def index
+    # This flash msg will persists 2 requests
+    flash[:notice] = "Logged in successfully"
+    
+    # This flash msg will persists only 1 request
+    flash.now[:alert] = "Invalid email or password"
   end
 end
