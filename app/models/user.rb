7b089b5f8411c285d6a746a5f94598011b3e08bd
Aldo Diaz <asaavedrajob@gmail.com>
2022-03-25 19:58:58 -0600

    RFB Part 12: Validations
    - rails db:migrate:redo
    => rollback and migrate in a single command

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,6 @@ class User < ApplicationRecord
   # has_secure_password is a rails method that will add the virtual fields password and password_confirmation to process the entered passwrod and fill the real field password_digest with an encrypted version of the string we assigned to the password/password_confirmation fields
   # has_secure_password requires the gem "bcrypt"
   has_secure_password
+
+  validates :email, presence: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: "must be a valid email address" }
 end
