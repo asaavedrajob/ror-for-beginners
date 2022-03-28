531c1a6e7503cbbcfa3a657c3872a7a12ef9d5c6
Aldo Diaz <asaavedrajob@gmail.com>
2022-03-28 11:20:05 -0600

    RFB Part 15: Login with Session Cookies

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,9 +1,13 @@
 class MainController < ApplicationController
   def index
     # This flash msg will persists 2 requests
-    flash[:notice] = "Logged in successfully"
+    # flash[:notice] = "Logged in successfully"
     
     # This flash msg will persists only 1 request
-    flash.now[:alert] = "Invalid email or password"
+    # flash.now[:alert] = "Invalid email or password"
+
+    if session[:user_id]
+      @user = User.find_by(id: session[:user_id])
+    end
   end
 end
