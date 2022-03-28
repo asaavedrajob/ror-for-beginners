ef5432be40ea2da27e0364f26bcbfa95bd4e236f
Aldo Diaz <asaavedrajob@gmail.com>
2022-03-28 12:18:47 -0600

    RFB Part 18: Accessing the Current User

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -5,9 +5,5 @@ class MainController < ApplicationController
     
     # This flash msg will persists only 1 request
     # flash.now[:alert] = "Invalid email or password"
-
-    if session[:user_id]
-      @user = User.find_by(id: session[:user_id])
-    end
   end
 end
