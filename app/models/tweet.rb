51e59ed9d37083002338869c6709ce4438a95d33
Aldo Diaz <asaavedrajob@gmail.com>
2022-03-29 16:48:19 -0600

    RFB Part 30: Tweet Validations
    - null equals operator (||=) means that if the value already exists, just leave it, but if is nil/empty then set the default value

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,4 +1,12 @@
 class Tweet < ApplicationRecord
   belongs_to :user
   belongs_to :twitter_account
+
+  validates :body, length: { minimum: 1, maximum: 280 }
+  validates :publish_at, presence: true
+
+  after_initialize do
+    # ||= (null equals operator) means that if the value already exists, just leave it, but if is nil/empty then set the default value
+    self.publish_at ||= 24.hours.from_now
+  end
 end
